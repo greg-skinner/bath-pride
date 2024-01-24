ae003f349a62251a8f6e480848602493c0c765e4
Greg Skinner <gregory.s.skinner@bath.edu>
2024-01-24 18:24:36 +0000

    Build the home page

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -9,11 +9,21 @@ const carouselData: ICarousel = _carouselData;
 
 export const Page: React.FC = () => (
   <div>
-    <div className={styles.content}>Bath Pride home page</div>
+    <div className={styles.content}>
+      <div className={styles.headline}>Bath Pride is finally here!</div>
+      <div className={styles.text}>
+        Bath Pride is finally coming to Bath as its own distinct event. With the
+        local community we are trying to build a pride that is long-lasting and
+        represents the entire queer community. Together, with you, we are going
+        to make a pride we can all be proud of.
+      </div>
+      <div className={styles.hashtag}>#BathPride</div>
+    </div>
   </div>
 );
 
 export const documentProps = {
-  description: 'The Bath Pride homepage',
+  description:
+    'Bath Pride is finally coming to Bath as its own distinct event. Together, with you, we are going to make a pride we can all be proud of',
   img: carouselData[0].file,
 };
--- src/renderer/+onRenderHtml.tsx
+++ src/renderer/+onRenderHtml.tsx
@@ -43,7 +43,6 @@ export const onRenderHtml = async (pageContext: PageContextServer) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
         <meta name="google" content="nositelinkssearchbox">
         <link rel="icon" sizes="192x192" href="${
           APP_CONFIG.BASE_URL
