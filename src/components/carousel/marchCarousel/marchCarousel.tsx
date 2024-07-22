ce660e186edaa8aad116dbe5878a871b18d68886
Greg Skinner <gregory.s.skinner@bath.edu>
2024-07-22 22:08:01 +0100

    Update to march documentation etc

--- src/components/carousel/marchCarousel/marchCarousel.tsx
+++ src/components/carousel/marchCarousel/marchCarousel.tsx
@@ -6,6 +6,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import _carouselData from '@assets/march-carousel/march-carousel.json';
+import _preMarchData from '@assets/march-carousel/route-to-march.json';
 import SwiperCore from 'swiper';
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -16,15 +17,20 @@ import { MarchCarouselPage } from './marchCarouselPage';
 import styles from './marchCarousel.module.scss';
 import classNames from 'classnames';
 
+const preMarchData: ICarousel = _preMarchData;
 const carouselData: ICarousel = _carouselData;
 
 export interface IMarchCarouselProps {
   className?: string;
+  preMarch?: boolean;
 }
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
-export const MarchCarousel: React.FC<IMarchCarouselProps> = ({ className }) => (
+export const MarchCarousel: React.FC<IMarchCarouselProps> = ({
+  className,
+  preMarch,
+}) => (
   <div className={classNames(className, styles.carousel)}>
     <Swiper
       modules={[Autoplay, Navigation, Pagination]}
@@ -35,7 +41,7 @@ export const MarchCarousel: React.FC<IMarchCarouselProps> = ({ className }) => (
       loop
       speed={800}
     >
-      {carouselData.map((item, idx) => (
+      {(preMarch ? preMarchData : carouselData).map((item, idx) => (
         <SwiperSlide key={item.file}>
           <MarchCarouselPage {...item} index={idx} />
         </SwiperSlide>
