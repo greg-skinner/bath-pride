dca288f8eaa2dbe56965d36f0161782a5a7fb195
Greg Skinner <gregory.s.skinner@bath.edu>
2025-02-23 12:26:13 +0000

    Redirect QR code to bath-pride-2024 instead of feedback

--- src/pages/qr-code/+Page.tsx
+++ src/pages/qr-code/+Page.tsx
@@ -4,6 +4,5 @@ export const Page: React.FC = () => null;
 
 export const documentProps: IMetaData = {
   title: 'Redirecting...',
-  redirectUrl:
-    'https://docs.google.com/forms/d/e/1FAIpQLScN3Ryi2itEa0G8h2jV74fvd-EmNxkWem2GNtI34AYh7J7TjA/viewform',
+  redirectUrl: 'bath-pride-2024',
 };
